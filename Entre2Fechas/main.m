c183a354ed0b3dcfa5dc373c49eef5f06cc82be4
Isaac Mac <isaacmac@Isaacs-iMac.local>
2016-09-15 16:23:02 +0200

    Initial Commit

---
+++ Entre2Fechas/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Entre2Fechas
+//
+//  Created by Isaac Mac on 15/9/16.
+//  Copyright (c) 2016 Isaac Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
